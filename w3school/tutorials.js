f6a6f6a1b8e3b5529de6acb442123f95fc7017dc
austin-wang <austin-wang@nec.com.tw>
2018-12-08 23:02:11 +0800

    add url query string

--- w3school/tutorials.js
+++ w3school/tutorials.js
@@ -1,6 +1,12 @@
 var http = require('http');
+var url = require('url');
+var dt = require('./myfirstmodule');
 
 http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type':'text/plain'});
-    res.end('Hello World!!!!');
+    var q = url.parse(req.url, true).query;
+    var text = q.year + " "+q.month;
+    res.write(req.url);
+    res.write("The date and time are current:"+dt.myDateTime());
+    res.end(text + ',Hello World!!!!');
 }).listen(8080);
