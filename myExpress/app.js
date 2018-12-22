90fc0b92c4f9447e9190e0898edb0b68faa756cc
austinwang104 <austin.wang@104.com.tw>
2018-12-22 20:36:08 +0800

    test async require

--- myExpress/app.js
+++ myExpress/app.js
@@ -2,5 +2,16 @@ const express = require('express');
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello world!'));
+const GoAsync = require('./go-async.js');
+
+app.get('/', (req, res) => {
+
+    //Node.js
+    console.log(async function () {});
+
+    GoAsync.run();
+
+    res.send('Hello world!');
+
+});
 app.listen(port, ()=>console.log(`Example app listening on port ${port}!`));
