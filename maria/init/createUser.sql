77fa86a6fe6b71c90cc0d6340f0e77e9d973169e
austinwang104 <austin.wang@104.com.tw>
2019-02-14 00:27:03 +0800

    createUser & grant sql udpate

--- maria/init/createUser.sql
+++ maria/init/createUser.sql
@@ -4,5 +4,7 @@ CREATE USER 'ehrms2_user' IDENTIFIED by '104104';
 GRANT USAGE ON *.* TO 'ehrms2_user'@localhost IDENTIFIED BY '104104';
 GRANT USAGE ON *.* TO 'ehrms2_user'@'%' IDENTIFIED BY '104104';
 GRANT ALL privileges ON `ehrms2`.* TO 'ehrms2_user'@localhost;
+GRANT ALL privileges ON `ehrms2`.* TO 'ehrms2_user'@'%';
+GRANT ALL privileges ON `ehrms2_log`.* TO 'ehrms2_user'@localhost;
 GRANT ALL privileges ON `ehrms2_log`.* TO 'ehrms2_user'@'%';
 FLUSH PRIVILEGES ;
