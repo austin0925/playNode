c0436f21ce7daaf6dcb130551f9e8e182a079b38
austinwang104 <austin.wang@104.com.tw>
2019-06-01 08:33:20 +0800

    [mysqldump] cmd update

---
+++ maria/init/white.sql
@@ -0,0 +1,9 @@
+\\172.19.5.22\e$\ehrms2_backup\DB_Backup
+cd C:\Program Files\MariaDB 10.2\bin
+mysql -u root -p --default-character-set=utf8 ehrms2 < D:\DB_ehrms2_2019_05_13_23_00.sql
+
+\\172.19.5.22\e$\ehrms2_backup\DB_Backup
+cd C:\Program Files\MariaDB 10.2\bin
+mysql -u root -p --default-character-set=utf8 ehrms2 < D:\DB_ehrms2_2019_05_13_23_00.sql
+mysql -u root -p --default-character-set=utf8 ehrms2_log < D:\DB_ehrms2_2019_05_13_23_00_log.sql
+https://remotedesktop.google.com/access/
